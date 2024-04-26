aab4cfea427e3d3dc9c7313148e7fe28c894ca80
Deathknight101 <120172437+Deathknight101@users.noreply.github.com>
2024-04-26 23:48:43 +0600

    added delete

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         })
+
+        app.delete('/artscraft/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await artsCraftsCollection.deleteOne(query);
+            res.send(result);
+        })
+
         
         app.put('/artscraft/:id',async(req,res)=>{
             const id = req.params.id;
